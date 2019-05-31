9e5eab10e2e76049a8ceda28856d873e63e096f2
dinukadev <dinuka.arseculeratne@gmail.com>
2019-05-31 14:52:46 +1000

    currying a method with one parameter group

--- src/CurryingApproach.scala
+++ src/CurryingApproach.scala
@@ -4,9 +4,21 @@ object CurryingApproach {
     a + b + c
   }
 
+  def curryingWithOneParamGroup(a:Int,b:Int)={
+    a+b
+  }
+
   def main(args: Array[String]) = {
     //You can also use curryingExampleOne(2)_
     val funcWithParamOne = curryingExampleOne(2)(_: Int)(1)
     println(funcWithParamOne(1))
+
+    val initialFunc = (curryingWithOneParamGroup _).curried
+
+    println((curryingWithOneParamGroup _).isInstanceOf[Function2[_,_,_]])
+    println(initialFunc(1)(2))
+
+
+
   }
 }

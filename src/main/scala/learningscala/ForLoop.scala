8dd3ea0d878cd560ca35e035fb87bde52a7522c5
dinukadev <dinuka.arseculeratne@gmail.com>
2019-06-03 14:23:47 +1000

    for comprehension scala

---
+++ src/main/scala/learningscala/ForLoop.scala
@@ -0,0 +1,17 @@
+package learningscala
+
+object ForLoop extends App {
+
+  case class Person(name: String)
+
+  val personList = List(Person("Dinuka"),Person("Roshan"))
+
+  val namesStartingWithD = for {
+    p <- personList
+    firstName = p.name
+    if(firstName startsWith "D")
+  } yield firstName.toUpperCase
+
+
+  println(namesStartingWithD)
+}

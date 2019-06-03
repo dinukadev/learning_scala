b1f939a5328f50435d9398f2b4e67892aca9f99b
dinukadev <dinuka.arseculeratne@gmail.com>
2019-06-03 15:18:05 +1000

    adding the map functionality

--- src/main/scala/learningscala/MySeqImpl.scala
+++ src/main/scala/learningscala/MySeqImpl.scala
@@ -1,25 +1,36 @@
 package learningscala
 
-import scala.collection.JavaConversions.`deprecated asJavaIterable`
 import scala.collection.mutable.ArrayBuffer
 
-object MySeqImpl extends App{
+object MySeqImpl extends App {
 
-  case class Sequence[A](initialElements: A*){
+  case class Sequence[A](initialElements: A*) {
     private val elemes = ArrayBuffer[A]();
 
     elemes ++= initialElements
 
-    def foreach(block: A => Unit): Unit ={
+    def foreach(block: A => Unit): Unit = {
       elemes.foreach(block)
+    }
+
+    def map[B](f: A => B): Sequence[B] = {
+      val abMap: ArrayBuffer[B] = elemes.map(f)
+
+      Sequence(abMap: _*)
     }
   }
 
-  val mySeq = new Sequence(1,2,3)
+  val mySeq = new Sequence(1, 2, 3)
 
   println(mySeq)
 
-  for(i<-mySeq){
+  for (i <- mySeq) {
     println(i)
   }
+
+  val multipleByTwo = for {
+    i <-mySeq
+  } yield  i*2
+
+  println(multipleByTwo)
 }
